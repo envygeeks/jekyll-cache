0cb0bd085285ff374652423eb092d588c614f7b6
Jordon Bedwell <jordon@envygeeks.io>
2017-12-22 23:10:34 -0600

    Cleanup.

--- lib/jekyll-cache.rb
+++ lib/jekyll-cache.rb
@@ -1,6 +1,48 @@
-# rubocop:disable Naming/FileName
 # Frozen-string-literal: true
-# Copyright: 2017 - MIT License
+# Copyright: 2017 - Jordon Bedwell - MIT License
 # Encoding: utf-8
 
-require "jekyll/cache"
+
+require "jekyll/sanity"
+require "jekyll/log_wrapper"
+require "jekyll"
+
+module Jekyll
+  module Cache
+    autoload :FileStore, "jekyll/cache/file_store"
+    autoload :MemoryStore, "jekyll/cache/memory_store"
+    autoload :NullStore, "jekyll/cache/null_store"
+
+    # --
+    # Clear the entire cache.
+    # @note Doesn't matter if it exists.
+    # @return [true,false]
+    # --
+    def self.clear
+      if Jekyll.cache_dir&.directory?
+        then Jekyll.cache_dir.rm_r
+      end
+    end
+
+    # --
+    # @return [Jekyll::LogWrapper]
+    # Creates a log wrapper for ActiveSupport
+    # @see Jekyll::LogWrapper
+    # --
+    def self.logger
+      logger = Jekyll.logger
+      @logger ||= LogWrapper.new(logger)
+    end
+  end
+
+  # --
+  # @note this should always use FileStore.
+  # A global cache that uses the FileStore. This is meant
+  #   for quick caching (or) one off caching.
+  # @return [FileStore] the new cache.
+  # --
+  def self.cache
+    key = "global"
+    @cache ||= Cache::FileStore.new(key)
+  end
+end
--- lib/jekyll/cache.rb
+++ lib/jekyll/cache.rb
@@ -2,42 +2,4 @@
 # Copyright: 2017 Jordon Bedwell - MIT License
 # Encoding: utf-8
 
-require "jekyll/sanity"
-require "jekyll/log_wrapper"
-require "jekyll"
-
-module Jekyll
-  module Cache
-    autoload :FileStore, "jekyll/cache/file_store"
-    autoload :MemoryStore, "jekyll/cache/memory_store"
-    autoload :NullStore, "jekyll/cache/null_store"
-
-    # --
-    # Clear the entire cache.
-    # @note Doesn't matter if it exists.
-    # @return [true,false]
-    # --
-    def self.clear
-      Jekyll.cache_dir.rm_r if Jekyll.cache_dir&.directory?
-    end
-
-    # --
-    # @return [Jekyll::LogWrapper]
-    # Creates a log wrapper for ActiveSupport
-    # @see Jekyll::LogWrapper
-    # --
-    def self.logger
-      @logger ||= LogWrapper.new(Jekyll.logger)
-    end
-  end
-
-  # --
-  # @note this should always use FileStore.
-  # A global cache that uses the FileStore. This is meant
-  #   for quick caching (or) one off caching.
-  # @return [FileStore] the new cache.
-  # --
-  def self.cache
-    @cache ||= Cache::FileStore.new("global")
-  end
-end
+require_relative "../jekyll-cache"
